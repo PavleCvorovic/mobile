5f651e0525a5aa0c92fd1f538d3e3cdd4ea3f04b
PavleCvorovic <pavle.cvorovic@ict-investment.me>
2021-03-23 14:21:12 +0100

    no message

--- src/app/servis.service.ts
+++ src/app/servis.service.ts
@@ -30,7 +30,7 @@ postovi:any=[];
         this.spiner=false;
         this.telefoni = posts;
         console.log(this.telefoni);
-        
+
 
       })
   }
@@ -56,66 +56,88 @@ postovi:any=[];
 
       })
   }
-  telefonBaza:any = {
+
+  post:any ={
+    tekst :'',
+    datum_objave:new Date()
+
+
+  }
+
+
+
+  dodajPost() {
+
+    const params = new HttpParams()
+
+
+    this.http
+      .post(
+        'http://localhost:8000/api/dodaj-telefon', this.post,
+        {
+          params: params
+        }
+      )
+  }
+
+
+
+    telefonBaza:any = {
     mark_id:1,
     cijena:'',
     opis:'',
     konfiguracije:'ssda',
     prodavac:'',
     kontakt:'',
-    datum_objave: new Date, 
+    datum_objave: new Date,
     datum_isteka: new Date,
     slika1:'https://tvtopsound.com/media/news/nijesmoMiOdJuceTvSerija/crop2/nijesmo-mi-od-juce-1.jpg',
     slika2:'https://tvtopsound.com/media/news/nijesmoMiOdJuceTvSerija/crop2/nijesmo-mi-od-juce-1.jpg',
     slika3:'https://tvtopsound.com/media/news/nijesmoMiOdJuceTvSerija/crop2/nijesmo-mi-od-juce-1.jpg'
   }
 
-  
-
-  
-
   dodajTelefon()
   {
     // this.knjiga = knjiga;
     // this.ostaliBaza = ostaliBaza;
      const params = new HttpParams()
- 
 
-    
+
+
     this.http
     .post(
-      'http://localhost:8000/api/dodaj-telefon',this.telefonBaza, 
+      'http://localhost:8000/api/dodaj-telefon',this.telefonBaza,
       {
         params:params
       }
     )
     .subscribe(responseData =>
-      { 
-        
+      {
+
       })
 
   }
 
-  
+
     model_ime:any;
 
- 
+
   dodajModel()
   {
     const params = new HttpParams()
- 
 
-    
+
+
     this.http
     .post(
-      'http://localhost:8000/api/dodaj-model',this.model_ime, 
+      'http://localhost:8000/api/dodaj-model',this.model_ime,
       {
         params:params
       }
     )
     .subscribe(responseData =>
-      { 
-        
+      {
+
       })
   }
 
