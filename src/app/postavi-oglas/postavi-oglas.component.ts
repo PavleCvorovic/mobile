4b3c9525b95e3ecbbb04fb5e4a32034a1012182d
Nikola Trifunovic <nikola.trifunovic@ict-investment.me>
2021-03-23 13:16:41 +0000

    nov

--- src/app/postavi-oglas/postavi-oglas.component.ts
+++ src/app/postavi-oglas/postavi-oglas.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import {ServisService} from '../servis.service';
 
 @Component({
@@ -7,13 +8,56 @@ import {ServisService} from '../servis.service';
   styleUrls: ['./postavi-oglas.component.css']
 })
 export class PostaviOglasComponent implements OnInit {
-cijena:string;
+
   constructor(public s:ServisService) { }
 
+  @ViewChild('f') signupForm: NgForm;
 
   ngOnInit(): void {
     this.s.dajmarku();
   }
+
+  marka='';
+  opis='';
+  cijena='';
+  prodavac='';
+  kontakt='';
+  konfiguracije='';
+  datum_objave= new Date;
+  datum_isteka= new Date;
+  slika1 = 'https://tvtopsound.com/media/news/nijesmoMiOdJuceTvSerija/crop2/nijesmo-mi-od-juce-1.jpg';
+  slika2 = 'https://tvtopsound.com/media/news/nijesmoMiOdJuceTvSerija/crop2/nijesmo-mi-od-juce-1.jpg';
+  slika3 ='https://tvtopsound.com/media/news/nijesmoMiOdJuceTvSerija/crop2/nijesmo-mi-od-juce-1.jpg';
+
+
+
+
+  onSubmit(signupForm:NgForm)
+  {
+    
+    // this.s.telefonBaza.cijena = this.cijena;
+    // this.s.telefonBaza.opis = this.opis;
+    // this.s.telefonBaza.prodavac= this.prodavac;
+    // this.s.telefonBaza.kontakt = this.kontakt;
+    // this.s.telefonBaza.mark_id = this.marka;
+    // this.s.telefonBaza.konfiguracije=this.konfiguracije;
+    // this.s.telefonBaza.datum_objave=this.datum_objave;
+    // this.s.telefonBaza.datum_isteka=this.datum_isteka;
+    // console.log(this.s.telefonBaza.datum_objave);
+    
+
+
+
+    // console.log(this.s.telefonBaza);
+    // this.s.dodajTelefon();
+    this.s.model_ime = this.cijena;
+    console.log(this.s.model_ime);
+    
+    this.s.dodajModel();
+    
+    
+  }
+
 
 
 
