e35bc8581ff8993517efcce0732a154275317269
PavleCvorovic <pavle.cvorovic@ict-investment.me>
2021-03-25 13:47:10 +0100

    no message

--- src/app/postavi-oglas/postavi-oglas.component.ts
+++ src/app/postavi-oglas/postavi-oglas.component.ts
@@ -11,7 +11,7 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./postavi-oglas.component.css']
 })
 export class PostaviOglasComponent implements OnInit {
-  
+promjena1:boolean=false;
 
   constructor(public s:ServisService) { }
 
@@ -38,7 +38,7 @@ export class PostaviOglasComponent implements OnInit {
 
   onSubmit(signupForm:NgForm)
   {
-    
+
      this.s.telefonBaza.cijena = this.cijena;
      this.s.telefonBaza.opis = this.opis;
      this.s.telefonBaza.prodavac= this.prodavac;
@@ -47,16 +47,37 @@ export class PostaviOglasComponent implements OnInit {
      this.s.telefonBaza.konfiguracije=this.konfiguracije;
       // console.log(this.datum_isteka.DatePipe.transform());
 
-    
-     this.s.dodajTelefon();
-    // this.s.models.model_ime = this.cijena;
-    // console.log(this.s.models.model_ime);
-    
-    // this.s.dodajModel();
-    
-    
-  }
 
+     this.s.dodajTelefon();
+     this.fun1()
+
+
+
+  }
+fun1(){
+if( this.cijena && this.opis && this.prodavac && this.kontakt && this.marka && this.konfiguracije != null){
+this.promjena1=true;
+
+this.cijena
+  this.opis='';
+  this.prodavac='';
+  this.konfiguracije='';
+  this.kontakt='';
+  this.marka='';
+  this.konfiguracije='';
+  this.cijena='';
+
+
+
+
+
+
+}
+else {
+  alert('Popunite sva polja u formi.')
+
+}
+}
 
 
 
