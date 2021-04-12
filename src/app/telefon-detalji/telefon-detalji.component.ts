375b50341307f3b8d08811d0e0510e487c270c87
HP\bojan <aleksavps4@gmail.com>
2021-04-12 14:38:59 +0200

    no message

--- src/app/telefon-detalji/telefon-detalji.component.ts
+++ src/app/telefon-detalji/telefon-detalji.component.ts
@@ -16,11 +16,11 @@ export class TelefonDetaljiComponent implements OnInit {
 provjera:number=this.id;
 
 ram_url:string= "assets/ram.png"
- cpu:string='https://img.flaticon.com/icons/png/512/1892/1892518.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF'
- mem:string='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp_I5LusD2YXWs2N8QJXO2PfXjQwMdAzO17Q&usqp=CAU'
- bat:string='https://lh3.googleusercontent.com/proxy/9efXkQXUdGy0Pe_Sd3vmzqyon58SRuINrIJ4DFps-cOTvTa3j88JS3SeEUeH2JejFy1W9KgEcuyEkCZpBL3YXEqKo8uqpcSmRngDozo10OI7EQ'
- cam:string='https://lh5.googleusercontent.com/proxy/SrFoEj_6qm6B0x6RFdoVE2HFO6AU8xnj3yrjsVFwwRxiYNy-Ju740Z9ioxramS_JuHNm_QMhT3KO3ULnMfHY_9H-KWw=s0-d'
- dis:string='https://img2.pngio.com/display-inches-mobile-phone-screen-size-smartphone-icon-display-size-png-512_512.png'
+ cpu:string='assets/cpu.png'
+ mem:string='assets/memoty.png'
+ bat:string='assets/battery.png'
+ cam:string='assets/camera.png'
+ dis:string='assets/display.png'
 
 
   ngOnInit(): void {
